26bc952b828de4fce0bc559a17f34bd74a2b7b62
Nikolai Murtazin <murtazinnikol@yandex.ru>
2024-06-08 14:06:27 +0200

    checkstyle: поправилпроблемы связынные с тестами

--- src/test/java/ru/yandex/practicum/filmorate/controller/JdbcFilmRepositoryTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/JdbcFilmRepositoryTests.java
@@ -25,65 +25,65 @@ class JdbcFilmRepositoryTests {
 
     private final JdbcFilmRepository filmRepository;
 
-//    @Test
-//    void testCreateAndFindFilmById() {
-//        Film film = Film.builder()
-//                .title("Начало")
-//                .description("Захватывающий триллер")
-//                .releaseDate(LocalDate.of(2010, 7, 16))
-//                .duration(148)
-//                .mpa(new MPA(1L, "PG-13"))
-//                .build();
-//
-//        Film createdFilm = filmRepository.create(film);
-//
-//        Optional<Film> filmOptional = filmRepository.getById(createdFilm.getId());
-//
-//        assertTrue(filmOptional.isPresent());
-//        assertThat(filmOptional).contains(createdFilm);
-//    }
-//
-//    @Test
-//    void testGetAllFilms() {
-//        Film film1 = Film.builder()
-//                .title("Начало")
-//                .description("Захватывающий триллер")
-//                .releaseDate(LocalDate.of(2010, 7, 16))
-//                .duration(148)
-//                .mpa(new MPA(1L, "PG-13"))
-//                .build();
-//        filmRepository.create(film1);
-//
-//        Film film2 = Film.builder()
-//                .title("Интерстеллар")
-//                .description("Путешествие через пространство и время")
-//                .releaseDate(LocalDate.of(2014, 11, 7))
-//                .duration(169)
-//                .mpa(new MPA(1L, "PG-13"))
-//                .build();
-//        filmRepository.create(film2);
-//
-//        Collection<Film> films = filmRepository.getAll();
-//        assertThat(films).hasSize(2);
-//    }
-//
-//    @Test
-//    void testUpdateFilm() {
-//        Film film = Film.builder()
-//                .title("Начало")
-//                .description("Захватывающий триллер")
-//                .releaseDate(LocalDate.of(2010, 7, 16))
-//                .duration(148)
-//                .mpa(new MPA(1L, "PG-13"))
-//                .build();
-//
-//        Film createdFilm = filmRepository.create(film);
-//
-//        createdFilm.setTitle("Начало Обновленное");
-//        filmRepository.update(createdFilm);
-//
-//        Optional<Film> filmOptional = filmRepository.getById(createdFilm.getId());
-//        assertTrue(filmOptional.isPresent());
-//        assertThat(filmOptional.get().getTitle()).isEqualTo("Начало Обновленное");
-//    }
+    @Test
+    void testCreateAndFindFilmById() {
+        Film film = Film.builder()
+                .title("Начало")
+                .description("Захватывающий триллер")
+                .releaseDate(LocalDate.of(2010, 7, 16))
+                .duration(148)
+                .mpa(new MPA(1L, "PG-13"))
+                .build();
+
+        Film createdFilm = filmRepository.create(film);
+
+        Optional<Film> filmOptional = filmRepository.getById(createdFilm.getId());
+
+        assertTrue(filmOptional.isPresent());
+        assertThat(filmOptional).contains(createdFilm);
+    }
+
+    @Test
+    void testGetAllFilms() {
+        Film film1 = Film.builder()
+                .title("Начало")
+                .description("Захватывающий триллер")
+                .releaseDate(LocalDate.of(2010, 7, 16))
+                .duration(148)
+                .mpa(new MPA(1L, "PG-13"))
+                .build();
+        filmRepository.create(film1);
+
+        Film film2 = Film.builder()
+                .title("Интерстеллар")
+                .description("Путешествие через пространство и время")
+                .releaseDate(LocalDate.of(2014, 11, 7))
+                .duration(169)
+                .mpa(new MPA(1L, "PG-13"))
+                .build();
+        filmRepository.create(film2);
+
+        Collection<Film> films = filmRepository.getAll();
+        assertThat(films).hasSize(2);
+    }
+
+    @Test
+    void testUpdateFilm() {
+        Film film = Film.builder()
+                .title("Начало")
+                .description("Захватывающий триллер")
+                .releaseDate(LocalDate.of(2010, 7, 16))
+                .duration(148)
+                .mpa(new MPA(1L, "PG-13"))
+                .build();
+
+        Film createdFilm = filmRepository.create(film);
+
+        createdFilm.setTitle("Начало Обновленное");
+        filmRepository.update(createdFilm);
+
+        Optional<Film> filmOptional = filmRepository.getById(createdFilm.getId());
+        assertTrue(filmOptional.isPresent());
+        assertThat(filmOptional.get().getTitle()).isEqualTo("Начало Обновленное");
+    }
 }
